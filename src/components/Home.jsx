fe9546ae49da71ed24a9864ea7719b0b74518ee4
Joelle  Johnson <joellej221@gmail.com>
2023-10-27 11:23:04 -0400

    more stylign done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,29 @@
 import { useState } from "react"
 
-function SingleMenuComponent(props){
+function SingleMenuComponent(props) {
     console.log(props)
     return (
-        <>
+        <div className="menu-item">
             <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </>
-        
+            <p id>{props.description}</p>
+        </div>
+
     )
 }
 
-export default function Home(){
+export default function Home() {
     const [menuItem, setMenuItem] = useState() //[getter(stateVar), setter]. This is destructuring the array. 
     // 1. fetch data -DONE lines 12-15
     // 2. get an array of objects -DONE line 4
 
     // 3. render each object
-        //create a state variable-DONE line 4
-        //put array of objects inside state variable-DONE line 22
-        //display the items in dom
-            //map over the array of objects
-            //return each item so that it displays on the browser
-
-    console.log('menuItem --->', menuItem)
+    //create a state variable-DONE line 4
+    //put array of objects inside state variable-DONE line 22
+    //display the items in dom
+    //map over the array of objects
+    //return each item so that it displays on the browser
 
     const handleButtonClick = () => {
-        console.log('hey class from a function')
 
         fetch('https://codice-boca.web.app/menu')
             .then(res => res.json()) //contacting the API
@@ -35,21 +32,20 @@ export default function Home(){
     }
 
     return (
-        <>
-            <button onClick={() => handleButtonClick()}>Get data</button>
-            {menuItem && 
+        <section className="menu-items">
+            {(!menuItem) //no menu items?
+                ?
+                //if no menu items, show button. 
+                <button onClick={() => handleButtonClick()}>Get data</button>
+                :
+                //Otherwise, show menu items.
                 menuItem.map((singleItem) => {
-                    console.log('singleItem---->', singleItem)
-            
-                return (
-                    <>
-                        <SingleMenuComponent title={singleItem.title} description={singleItem.description}/>
-                        {/* <h2>{singleItem.title}</h2>
-                        <p>{singleItem.description}</p> */}
-                    </>
-                )
-            })}
-            
-        </>
+                    return (
+                            <SingleMenuComponent title={singleItem.title} description={singleItem.description} />
+                    )
+                })
+            }
+
+        </section>
     )
 }
